ff1a573032dacfb55fcfced705d826d674934521
Shahed Nasser <shahednasser@gmail.com>
2022-03-08 19:02:06 +0200

    revert changes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { Module } from "medusa-extender";
-import { ProductModule } from "./modules/product/product.module";
-import { StoreModule } from "./modules/store/store.module";
-import { UserModule } from "./modules/user/user.module";
-
-@Module({
-  imports: [StoreModule, UserModule, ProductModule],
-})
-export class MarketplaceModule {}
+export { StoreModule } from "./modules/store/store.module";
+export { UserModule } from "./modules/user/user.module";
+export { ProductModule } from "./modules/product/product.module";
